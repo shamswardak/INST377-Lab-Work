cb3a9ae0a2b505dd8279395967113e80227d31e1
shamswardak <wardakshams@gmail.com>
2023-04-15 22:19:37 -0400

    add map markers

--- client/lab_8/scripts.js
+++ client/lab_8/scripts.js
@@ -33,18 +33,37 @@ function cutRestaurantList(list) {
 }
 
 function initMap() {
-  const carto = L.map("map").setView([51.505, -0.09], 13);
+  const carto = L.map("map").setView([38.98, -76.93], 13);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(carto);
+  return carto;
+}
+
+function markerPlace(array, map) {
+    console.log('array for markers', array);
+    
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+      });
+
+    array.forEach((item) => {
+        console.log('markerPlace', item);
+        const {coordinates} = item.geocoded_column_1;
+
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    })
 }
 
 async function mainEvent() {
   // the async keyword means we can make API requests
   const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
   const loadDataButton = document.querySelector("#data_load");
+  const clearDataButton = document.querySelector("#data_clear");
   const generateListButton = document.querySelector("#generate");
   const textField = document.querySelector("#resto");
 
@@ -52,11 +71,11 @@ async function mainEvent() {
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
 
-  initMap();
+  const carto = initMap();
 
   const storedData = localStorage.getItem("storedData");
-  const parsedData = JSON.parse(storedData);
-  if (parsedData.length > 0) {
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0) {
     generateListButton.classList.remove("hidden");
   }
 
@@ -76,6 +95,11 @@ async function mainEvent() {
     // This changes the response from the GET into data we can use - an "object"
     const storedList = await results.json();
     localStorage.setItem("storedData", JSON.stringify(storedList));
+    parsedData = storedList;
+
+    if (parsedData?.length > 0) {
+        generateListButton.classList.remove("hidden");
+      }
 
     loadAnimation.style.display = "none";
     // console.table(storedList);
@@ -86,6 +110,7 @@ async function mainEvent() {
     currentList = cutRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
+    markerPlace(currentList, carto);
   });
 
   textField.addEventListener("input", (event) => {
@@ -93,7 +118,14 @@ async function mainEvent() {
     const newList = filterList(currentList, event.target.value);
     console.log(newList);
     injectHTML(newList);
+    markerPlace(newList, carto);
   });
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log('clear browser data');
+    localStorage.clear();
+    console.log('localStorage Check', localStorage.getItem("storedData"))
+  })
 }
 
 document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
